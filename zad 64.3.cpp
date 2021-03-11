f1f269401da0d648088a3f41eab0e810f5c5f2b5
JakubRudniks <62304977+JakubRudniks@users.noreply.github.com>
2021-03-11 12:02:09 +0100

    Add files via upload

---
+++ zad 64.3.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <stdlib.h>
+#include <bitset>
+#include <cstdlib>
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+using namespace std;
+
+int main(int argc, char** argv) {
+	
+	    ifstream p;
+    p.open("D:\\FX TEAM\\dane_obrazki.txt");
+    char c;
+    int x;
+    int y;
+    int z;
+    bool i=false;
+	bool j=false;
+ 
+    for(int z=0; z<10; z++)
+    {
+        string tab[10];
+        file>>tab[10];
+        cout << tab[10] << endl;
+    }
+	
+{
+    for(int i = 0;i<10;++i){
+        for(int j=0;j<10;++j){
+            if(!(scren[i][j]==scren[i+10][j] && scren[i][j]==scren[i][j+10] && scren[i][j]==scren[i+10][j+10]))
+			
+            }
+        }
+    }
+    t[i][j] <<c;
+    cout<<t[i][j]<<" ";
+    }
+    cout <<endl;
+    }
+   if(i&&j==true)
+   {
+   	cout<<"Obrazek jest rowny"<<endl;
+   }
+}
+
+{
+	if (liczba_bledow> max_liczba_bledow) max_liczba_bledow = liczba_bledow;
+	
+	{
+		cout<<"Pokazuj liczbe bledow"<<endl;
+	}
+}
+
+{
+	 for(int i=0;i<n;i++)
+                if(tab[i]%2) 
+                        cout<<tab[i]<<" "; 
+        
+        cout<<endl;
+}
+    getchar();
+	
+	
+	
+return true;
+}
